a516dafe741bf221a4930217dbf5067b74a76166
zandermmcg <zandermmcg@gmail.com>
2025-02-15 18:02:19 -0600

    Added website title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>Chordify - Chord Progression Generator</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
